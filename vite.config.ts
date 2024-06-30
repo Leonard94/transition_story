d402dc5967978db5115147b0e4a4a87a657df40d
Владислав Коледа <vladislav.koleda94@gmail.com>
2024-06-30 20:41:00 +0300

    change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/transition_story/'
+  base: '/transition_story/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
